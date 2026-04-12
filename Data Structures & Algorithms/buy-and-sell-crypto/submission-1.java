101a58ef1d0c7236ee65687e554c8ee22dd3cc6b
ankitksde <ankitk.sde@gmail.com>
2026-04-12 22:58:45 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        int maxProfit=0;
+        int l=0;
+        int r=1;
+        while(r<n){
+            if(prices[r]>prices[l]){
+                maxProfit=Math.max(maxProfit, prices[r]-prices[l]);
+            }else l=r;
+            r++;
+        }
+        return maxProfit;
+    }
+}
